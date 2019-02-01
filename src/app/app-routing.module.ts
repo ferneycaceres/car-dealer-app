731f82a5a8bfd313460b5569ee5d5ca97e1d0753
ferneycaceres <fcaceres@portalfinance.co>
2019-02-01 17:15:03 -0500

    comparison view mocked

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ComparisonComponent } from './ui/components/comparison/comparison.component';
+import { LandingPageComponent } from './ui/components/landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent, pathMatch: 'full' },
+  { path: 'comparison', component: ComparisonComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ComparisonComponent } from './components/comparison/comparison.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @NgModule({
-  declarations: [LayoutComponent, HeaderComponent, FooterComponent],
+  declarations: [LayoutComponent, HeaderComponent, FooterComponent, ComparisonComponent, LandingPageComponent],
   imports: [
     CommonModule
   ],
