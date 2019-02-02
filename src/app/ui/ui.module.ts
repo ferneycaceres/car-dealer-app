c37c7267db8486f6d2a32ef80be8b553e012d70b
ferneycaceres <ferneycaceres@gmail.com>
2019-02-02 10:33:25 -0500

    router working

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -5,11 +5,13 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ComparisonComponent } from './components/comparison/comparison.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [LayoutComponent, HeaderComponent, FooterComponent, ComparisonComponent, LandingPageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [LayoutComponent]
 })
